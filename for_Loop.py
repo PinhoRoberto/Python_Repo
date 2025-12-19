7332b7ab707edb7909e598a5292812ed64490694
Roberto Pinho <roberto.pinho@protonmail.com>
2025-12-19 08:52:27 -0300

    Add multiplication table generator with for loop examples

---
+++ for_Loop.py
@@ -0,0 +1,31 @@
+number = int (input("Enter a number: "))
+
+for i in range(1,11):
+    result = number*i
+    print (f"{number} x {i} = {result}")
+
+    
+
+# While loop
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+# # For loop (cleaner!)
+# for count in range(5):
+#     print(count)
+
+# for i in range(5):
+#     print(i)
+
+
+# for i in range (1, 11):
+#     print(i)
+
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+# print("Liftoff! 🚀")
+
