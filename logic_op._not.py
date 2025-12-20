53e86bdce2da71b1f0bc02438053589081324e79
Roberto Pinho <roberto.pinho@protonmail.com>
2025-12-20 14:41:10 -0300

    Add logical NOT operator example

---
+++ logic_op._not.py
@@ -0,0 +1,9 @@
+senha_correta = "1234"
+senha = input('Digite a senha: ')
+if senha == str(senha_correta):
+    print('Senha correta')  
+if senha != str(senha_correta):
+    print('Senha incorreta')
+if not senha :
+    print('Não digitou Senha')
+
