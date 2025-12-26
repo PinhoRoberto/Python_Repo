daa71e6ac0444b3b23031255ff1de328f6bcba3f
Roberto Pinho <roberto.pinho@protonmail.com>
2025-12-26 14:25:48 -0300

    Update count_ten.py to demonstrate for loop as cleaner alternative

--- count_ten.py
+++ count_ten.py
@@ -1,8 +1,8 @@
 # While loop
-count = 0
-while count < 10:
-    print(count)
-    count += 1
+# count = 0
+# while count < 10:
+#     print(count)
+#     count += 1
 
 # For loop (cleaner!)
 for count in range(10):
