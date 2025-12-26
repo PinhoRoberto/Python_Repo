4eae4698cd159a1d06c3183205ccd1386dd1c012
Roberto Pinho <roberto.pinho@protonmail.com>
2025-12-26 14:26:10 -0300

    Add interactive command loop with quit functionality

---
+++ while_true.py
@@ -0,0 +1,8 @@
+while True:
+    command = input("Enter command (quit to exit): ")
+    
+    if command == "quit":
+        print("Goodbye!")
+        break
+    
+    print(f"You typed: {command}")
